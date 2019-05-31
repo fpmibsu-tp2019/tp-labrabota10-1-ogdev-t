268b2730ab5ea9c8431d69f5b5ba77b87c0e0786
artloko <artem1102zx@gmail.com>
2019-05-31 23:30:20 +0300

    created data model

---
+++ checkITin/checkITin/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  checkITin
+//
+//  Created by artloko on 5/31/19.
+//  Copyright © 2019 artloko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
