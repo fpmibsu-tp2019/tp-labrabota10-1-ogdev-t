99012e84fcddfb49b743e1099b4080788522ab65
artloko <artem1102zx@gmail.com>
2019-06-01 09:16:27 +0300

    Added view controllers to the storyboard

---
+++ checkITin/checkITin/Controllers/AchieventViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AchieventViewController.swift
+//  checkITin
+//
+//  Created by artloko on 6/1/19.
+//  Copyright © 2019 artloko. All rights reserved.
+//
+
+import UIKit
+
+class AchieventViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
